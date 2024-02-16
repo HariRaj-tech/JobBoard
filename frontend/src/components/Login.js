60fd912f94229879df3e5914e14a22fd7954fcab
vikasreddy1406 <vikassai345@gmail.com>
2024-02-16 17:05:47 +0530

    updated home page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,11 +10,14 @@ export default function Login() {
 
     return (
 
-        <div className="container mx-auto px-4 py-8">
-            <div className="max-w-md mx-auto">
-                <h2 className="text-3xl font-bold text-center mb-4">Login</h2>
-                <form className="space-y-4">
-                    {/* <div className="flex items-center mb-2">
+        
+
+
+        <div className="container mx-auto px-4 py-8 bg-[#C6DCBA] h-screen">
+                <div className="max-w-md mx-auto">
+                    <h2 className="text-3xl font-bold text-center mb-4">Login</h2>
+                    <form className="space-y-4">
+                        {/* <div className="flex items-center mb-2">
                         <input
                             type="radio"
                             id="user"
@@ -36,7 +39,7 @@ export default function Login() {
                         />
                         <label htmlFor="company" className="text-sm font-medium">Company</label>
                     </div> */}
-                    {/* {role === 'user' && (
+                        {/* {role === 'user' && (
                         <div>
                             <div className="mb-2">
                                 <label htmlFor="userEmail" className="block text-sm font-medium">
@@ -88,35 +91,37 @@ export default function Login() {
                             </div>
                         </div>
                     )} */}
-                    <div>
-                        <div className="mb-2">
-                            <label htmlFor="userEmail" className="block text-sm font-medium">
-                                Email address
-                            </label>
-                            <input
-                                type="email"
-                                id="userEmail"
-                                placeholder="Enter your email address"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
-                            />
+                        <div>
+                            <div className="mb-2">
+                                <label htmlFor="userEmail" className="block text-sm font-medium">
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    id="userEmail"
+                                    placeholder="Enter your email address"
+                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                                />
+                            </div>
+                            <div className="mb-2">
+                                <label htmlFor="userPassword" className="block text-sm font-medium">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="userPassword"
+                                    placeholder="Enter your password"
+                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
+                                />
+                            </div>
                         </div>
-                        <div className="mb-2">
-                            <label htmlFor="userPassword" className="block text-sm font-medium">
-                                Password
-                            </label>
-                            <input
-                                type="password"
-                                id="userPassword"
-                                placeholder="Enter your password"
-                                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-lg block w-full p-2.5"
-                            />
-                        </div>
-                    </div>
-                    <button type="submit" className="inline-flex items-center px-4 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
-                        Sign in
-                    </button>
-                </form>
+                        <button type="submit" className="inline-flex items-center px-4 py-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                            Sign in
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
+
+        
     )
 }
