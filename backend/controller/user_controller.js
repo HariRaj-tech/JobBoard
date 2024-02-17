6512582edbefeb966df094fa0b7c565871661a10
shifu-dev <csingh8476@gmail.com>
2024-02-17 18:26:53 +0530

    update: fix user api.

--- backend/controller/user_controller.js
+++ backend/controller/user_controller.js
@@ -1,136 +1,101 @@
-const { users } = require("./modal/users");
+const { users } = require("../modal/user_database");
+const statusCodes = require('http-status-codes').StatusCodes
 
-// const status_codes = require('http-status-codes').StatusCodes
-// const user_manager = require('./user_manager')
-
-// function get_users(req, res) {
-//     console.log('get users request recieved.')
-//     res.send(user_manager.get_users());
-// }
-
-// function get_user(req, res) {
-//     console.log('get user request recieved.')
-//     res.send('not implemented yet.')
-// }
-
-exports.checkAuth = async (req, res) => {
+exports.auth = async (req, res) => {
     try {
-      
-      const sessionUser = req.session.user;
-      
-      // Check if the user session exists
-      if (!sessionUser) {
-        return res.status(401).send({ status: false, message: "Unauthorized" });
-      }
-  
-      // Find user in the dummy database based on email
-      const foundUser = users.find(u => u.email === sessionUser.email);
-      if (!foundUser) {
-        return res.status(401).send({ status: false, message: "User not found" });
-      }
-  
-      // Return user information
-      return res.status(200).send({ status: true, user: foundUser });
+        const sessionUser = req.session.user;
+
+        // Check if the user session exists
+        if (!sessionUser) {
+            return res.status(statusCodes.UNAUTHORIZED)
+                .send({ status: false, message: "unauthorized." });
+        }
+
+        // Find user in the dummy database based on email
+        const foundUser = users.find(u => u.email === sessionUser.email);
+        if (!foundUser) {
+            return res.status(statusCodes.UNAUTHORIZED)
+                .send({ status: false, message: "user not found." });
+        }
+
+        // Return user information
+        return res.status(statusCodes.OK)
+            .send({ status: true, user: foundUser });
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR)
+            .send({ status: false, message: "internal server error." });
+    }
+};
+
+exports.signup = async (req, res) => {
+    try {
+
+        console.log('user create request recieved.')
+
+        const { name, email, password } = req.body;
+        if (!email) {
+            return res.status(statusCodes.BAD_REQUEST)
+                .send("invalid user request.");
+        }
+
+        // Check if the email already exists
+        const existingUser = users.find((user) => user.email === email);
+        if (existingUser) {
+            return res.status(statusCodes.BAD_REQUEST)
+                .send("user account with this email already exists.");
+        }
+
+        // Create a new user
+        const newUser = { id: users.length + 1, name, email, password };
+        users.push(newUser);
+
+        // Simulate session creation
+        // todo: fix this.
+        // req.session.user = newUser;
+
+        console.log('user created successfully.')
+
+        return res.status(statusCodes.OK)
+            .send({ status: true, user: newUser });
     } catch (err) {
-      console.error(err);
-      return res.status(500).send({ status: false, message: "Internal server error" });
+        console.error(err);
+        return res.status(statusCodes.INTERNAL_SERVER_ERROR)
+            .send("internal server error");
     }
-  };
-
-exports.signUpUser = async (req, res) => {
-  try {
-    const { fullName, email, password } = req.body;
-    if (!email) {
-      return res.status(400).send("Invalid user request");
-    }
-
-    // Check if the email already exists
-    const existingUser = users.find((user) => user.email === email);
-    if (existingUser) {
-      return res.status(400).send("This email already exists");
-    }
-
-    // Create a new user
-    const newUser = { id: users.length + 1, fullName, email, password };
-    users.push(newUser);
-
-    // Simulate session creation
-    req.session.user = newUser;
-
-    return res.status(200).send({ status: true, user: newUser });
-  } catch (err) {
-    console.error(err);
-    return res.status(500).send("Internal server error");
-  }
 };
 
-exports.loginUser = async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
+exports.login = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  if (!email || !password) {
-    return res
-      .status(400)
-      .send({ status: false, message: "Email and Password are required" });
-  }
-
-  try {
-    // Find user in the dummy database based on email and password
-    const user = users.find(
-      (user) => user.email === email && user.password === password
-    );
-    if (!user) {
-      return res
-        .status(400)
-        .send({ status: false, message: "User doesn't exist" });
+    if (!email || !password) {
+        return res
+            .status(statusCodes.BAD_REQUEST)
+            .send({ status: false, message: "email and password are required." });
     }
 
-    // Simulate session creation
-    req.session.user = user;
+    try {
+        // Find user in the dummy database based on email and password
+        const user = users.find(
+            (user) => user.email === email && user.password === password
+        );
+        if (!user) {
+            return res
+                .status(statusCodes.BAD_REQUEST)
+                .send({ status: false, message: "user doesn't exist." });
+        }
 
-    return res.status(200).send({ status: true, user: user });
-  } catch (err) {
-    console.error(err);
-    return res
-      .status(500)
-      .send({ status: false, message: "Internal server error" });
-  }
+        // Simulate session creation
+        req.session.user = user;
+
+        return res.status(statusCodes.OK)
+            .send({ status: true, user: user });
+    } catch (err) {
+        console.error(err);
+        return res
+            .status(statusCodes.INTERNAL_SERVER_ERROR)
+            .send({ status: false, message: "internal server error." });
+    }
 };
-
-// async function create_user(req, res) {
-//   console.log("create user request recieved.");
-
-//   todo: check request body.
-
-//   const user_details = {
-//     name: req.body.name,
-//     password: req.body.password,
-//   };
-
-//   const user_id = await user_manager.create_user(user_details);
-//   if (user_id == null) {
-//     res.status(status_codes.INTERNAL_SERVER_ERROR).send("failed to add user");
-
-//     return;
-//   }
-
-//   res.status(status_codes.OK).send(`${user_id}`);
-// }
-
-// function update_user(req, res) {
-//   console.log("update user request recieved.");
-//   res.send("not implemented yet.");
-// }
-
-// function delete_user(req, res) {
-//   console.log("delete user request recieved.");
-//   res.send("not implemented yet.");
-// }
-
-// module.exports = {
-//   get_users,
-//   get_user,
-//   create_user,
-//   update_user,
-//   delete_user,
-// };
--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,17 +1,9 @@
 const express = require("express");
-const controller = require("./controller/user_controller");
-
+const controller = require("../controller/user_controller");
 const router = express.Router();
 
-// router.get("/", controller.get_users);
-// router.get("/:userId", controller.get_user);
-// router.post("/", controller.create_user);
-// router.put("/:userId", controller.update_user);
-// router.delete("/:userId", controller.delete_user);
-
-router
-  .post("/signup", signUpUser)
-  .post("/login", loginUser)
-  .get("/check", checkAuth);
+router.post("/signup", controller.signup)
+router.post("/login", controller.login)
+router.get("/check", controller.auth);
 
 exports.router = router;
