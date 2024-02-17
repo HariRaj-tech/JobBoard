7d1a270efd5c77446df85d712bde23f9832ee24f
sureshkumar-n <sureshsk7323@gmail.com>
2024-02-17 12:12:10 +0530

    job apply page media query added

--- frontend/src/components/JobPages/Applyjob.js
+++ frontend/src/components/JobPages/Applyjob.js
@@ -1,15 +1,19 @@
-import headerimg from '../../asset/thumb.png';
-import briefcaseimg from '../../asset/briefcase.svg';
-import timeimg from '../../asset/time.svg';
-import industryimg from '../../asset/industry.svg';
-import joblevelimg from '../../asset/job-level.svg';
-import salaryimg from '../../asset/salary.svg';
-import experienceimg from '../../asset/experience.svg';
-import jobtypeimg from '../../asset/job-type.svg';
-import deadlineimg from '../../asset/deadline.svg';
-import updateimg from '../../asset/updated.svg';
-import locationimg from '../../asset/location.svg';
-import companylogo from '../../asset/avatar.png';
+import headerimg from '../../assets/thumb.png';
+import briefcaseimg from '../../assets/briefcase.svg';
+import timeimg from '../../assets/time.svg';
+import industryimg from '../../assets/industry.svg';
+import joblevelimg from '../../assets/job-level.svg';
+import salaryimg from '../../assets/salary.svg';
+import experienceimg from '../../assets/experience.svg';
+import jobtypeimg from '../../assets/job-type.svg';
+import deadlineimg from '../../assets/deadline.svg';
+import updateimg from '../../assets/updated.svg';
+import locationimg from '../../assets/location.svg';
+import companylogo from '../../assets/avatar.png';
+import fb from '../../assets/fb.svg';
+import x from '../../assets/x.svg';
+import reddit from '../../assets/rd.svg';
+import wa from '../../assets/wa.svg';
 import './Applyjob.css';
 function Applyjob() {
 
@@ -135,10 +139,50 @@ function Applyjob() {
                             </div>
                         </div>
                         <div className="about-company">
-
+                            <h4>Welcome to AliStudio Team</h4>
+                            <p>The AliStudio Design team has a vision to establish a trusted platform that enables productive and healthy enterprises in a world of digital and remote everything, constantly changing work patterns and norms, and the need for organizational resiliency.</p>
+                            <p>The ideal candidate will have strong creative skills and a portfolio of work which demonstrates their passion for illustrative design and typography. This candidate will have experiences in working with numerous different design platforms such as digital and print forms.</p>
+                            <h4>Essential Knowledge, Skills, and Experience</h4>
+                            <ul>
+                                <li>A portfolio demonstrating well thought through and polished end to end customer journeys</li>
+                                <li>5+ years of industry experience in interactive design and / or visual design</li>
+                                <li>Excellent interpersonal skills</li>
+                                <li>Aware of trends inmobile, communications, and collaboration</li>
+                                <li>Ability to create highly polished design prototypes, mockups, and other communication artifacts</li>
+                                <li>The ability to scope and estimate efforts accurately and prioritize tasks and goals independently</li>
+                                <li>History of impacting shipping products with your work</li>
+                                <li>A Bachelor's Degree in Design (or related field) or equivalent professional experience</li>
+                                <li>Proficiency in a variety of design tools such as Figma, Photoshop, Illustrator, and Sketch</li>
+                            </ul>
+                            <h4>Preferred Experience</h4>
+                            <ul>
+                                <li>Designing user experiences for enterprise software / services</li>
+                                <li>Creating and applying established design principles and interaction patterns</li>
+                                <li>Aligning or influencing design thinking with teams working in other geographies</li>
+                            </ul>
+                            <h4>Product Designer</h4>
+                            <p><strong>Product knowledge: </strong>Deeply understand the technology and features of the product area to which you are assigned.</p>
+                            <p><strong>Research: </strong>Provide human and business impact and insights for products.</p>
+                            <p><strong>Deliverables: </strong>Create deliverables for your product area (for example competitive analyses, user flows, low fidelity wireframes, high fidelity mockups, prototypes, etc.) that solve real user problems through the user experience.</p>
+                            <p><strong>Communication: </strong>Communicate the results of UX activities within your product area to the design team department, cross-functional partners within your product area, and other interested Superformula team members using clear language that simplifies complexity.</p>
+                        </div>
+                        <div className="col1-footer">
+                            <span>AliThemes</span>
                         </div>
                         <div className="media-container">
-
+                            <div>
+                                <div>
+                                    <button className="apply-btn" style={{"margin":"10px 10px"}}>Apply now</button>
+                                    <button className="apply-btn" style={{"background":"#ffff","border": "1px solid #b4c0e0","color":"#4f5e64","font-size":"16px","line-height":"26px","font-weight": "700"}}>Save job</button>
+                                </div>
+                                <div className="media">
+                                    <h5 style={{"font-size":"16px","font-weight": "600"}}>Share this</h5>
+                                    <span className="media-icon"><img src={fb} alt="facebook"/></span>
+                                    <span className="media-icon"><img src={x} alt="facebook"/></span>
+                                    <span className="media-icon"><img src={reddit} alt="facebook"/></span>
+                                    <span className="media-icon"><img src={wa} alt="facebook"/></span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="col2">
@@ -153,22 +197,29 @@ function Applyjob() {
                                 </div>
                             </div>
                             <div className="address-container">
-                                <iframe
-                                    title="Embedded Map"
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15668.413292258672!2d77.9624324!3d10.955567299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baa2847d35ff6ab%3A0x564660af0ef61a8!2sVSB%20Engineering%20College!5e0!3m2!1sen!2sin!4v1708086070363!5m2!1sen!2sin"
-                                    width="200"
-                                    height="200"
-                                    style={{ border: 0 }}
-                                    allowFullScreen
-                                    loading="lazy"
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                ></iframe>
+                                <div>
+                                    <iframe
+                                        className="map"
+                                        title="Embedded Map"
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15668.413292258672!2d77.9624324!3d10.955567299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3baa2847d35ff6ab%3A0x564660af0ef61a8!2sVSB%20Engineering%20College!5e0!3m2!1sen!2sin!4v1708086070363!5m2!1sen!2sin"
+                                        width="200"
+                                        height="200"
+                                        style={{ border: 0 }}
+                                        allowFullScreen
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                    ></iframe>
+                                </div>
                                 <ul>
-                                   <li>205 North Michigan Avenue, Suite 810 Chicago, 60601, USA</li>
-                                   <li>Phone: (123) 456-7890</li>
-                                   <li>Email: contact@Evara.com</li>
+                                    <li>205 North Michigan Avenue, Suite 810 Chicago, 60601, USA</li>
+                                    <li>Phone: (123) 456-7890</li>
+                                    <li>Email: contact@Evara.com</li>
                                 </ul>
                             </div>
+                        </div>
+                        <div className="jobs-card">
+                            <h5 style={{"font-size":"18px"}}>Similar jobs</h5>
+                            
                         </div>
                     </div>
                 </div>
