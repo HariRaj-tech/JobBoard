6008a854729e9e1ca5978725fc798170215ddf10
vikasreddy1406 <vikassai345@gmail.com>
2024-02-24 19:48:21 +0530

    created job posting page

--- frontend/src/pages/recruiters/Postnewjob.js
+++ frontend/src/pages/recruiters/Postnewjob.js
@@ -53,7 +53,7 @@ export default function Postnewjob() {
             <div className="max-w-md mx-auto">
                 <h4 className='text-center'>Post New Job</h4>
                 <form className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4' onSubmit={handleSubmit}>
-                    
+
                     <div className=''>
                         <label htmlFor="industry" className='block text-gray-700 text-sm font-bold mb-2'>Industry *</label>
                         <input type="text" name="industry" id="industry" onChange={handleInputChange} required className="shadow appearance-none border border-gray-800 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-8" />
