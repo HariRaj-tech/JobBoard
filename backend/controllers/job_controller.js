e79e1cb1c94b7ad2e01a3658b3cb46143f07b79e
shifu-dev <csingh8476@gmail.com>
2024-02-26 01:33:02 +0530

    update: updated job post api.

--- backend/controllers/job_controller.js
+++ backend/controllers/job_controller.js
@@ -1,10 +1,12 @@
-const pool = require('models/job');
+const jobs = require('models/job');
 const logger = require('services/logger');
+const status_codes = require('http-status-codes').StatusCodes;
 
-exports.post = (req, res) => {
+exports.post = async (req, res) => {
     logger.info('job create request recieved.');
 
-    const jobDetails = {
+    const job_details = {
+        title: 'job-title',
         industry: req.body.industry,
         designation: req.body.designation,
         experience: req.body.experience,
@@ -14,16 +16,22 @@ exports.post = (req, res) => {
         skills: req.body.skills,
     };
 
-    // todo: add job into database, and return the unique id
+    console.assert(job_details.industry);
+    console.assert(job_details.designation);
+    console.assert(job_details.experience);
+    console.assert(job_details.salary);
+    console.assert(job_details.location);
+    console.assert(job_details.deadline);
+    console.assert(job_details.skills);
 
-    logger.info('job created successfully.');
+    const job = await jobs.create(job_details);
+    logger.info('job created successfully.', job.toJSON());
+
+    return res.status(status_codes.OK).send({ job: job.toJSON() });
 };
 
 exports.getById = (req, res) => {
     const jobId = req.body.jobId;
 
     logger.info('job get request recieved.');
-
-    pool.query('SELECT FROM DATB');
-    logger.info('job created successfully.');
 };
