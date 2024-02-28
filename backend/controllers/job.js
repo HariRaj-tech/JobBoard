39a9c3c9e1aed08ac930355bf59d229e81a4a928
Chetan Singh Sajwan <csingh8476@gmail.com>
2024-02-28 19:59:46 +0530

    update: update job api.

--- backend/controllers/job.js
+++ backend/controllers/job.js
@@ -1,36 +1,51 @@
 const jobs = require('models/job');
 const logger = require('services/logger');
-const status_codes = require('http-status-codes').StatusCodes;
+const statusCodes = require('http-status-codes').StatusCodes;
 
 exports.post = async (req, res) => {
     logger.info('job create request recieved.');
 
-    const job_details = {
+    const jobDetails = {
         title: req.body.title,
+        type: req.body.type,
+        level: req.body.level,
         industry: req.body.industry,
         experience: req.body.experience,
         salary: req.body.salary,
         location: req.body.location,
         deadline: req.body.deadline,
         skills: req.body.skills,
+        about: req.body.about,
     };
 
-    console.assert(job_details.industry);
-    console.assert(job_details.designation);
-    console.assert(job_details.experience);
-    console.assert(job_details.salary);
-    console.assert(job_details.location);
-    console.assert(job_details.deadline);
-    console.assert(job_details.skills);
+    console.assert(jobDetails.title);
+    console.assert(jobDetails.type);
+    console.assert(jobDetails.level);
+    console.assert(jobDetails.industry);
+    console.assert(jobDetails.experience);
+    console.assert(jobDetails.salary);
+    console.assert(jobDetails.location);
+    console.assert(jobDetails.deadline);
+    console.assert(jobDetails.skills);
 
-    const job = await jobs.create(job_details);
+    const job = await jobs.create(jobDetails);
     logger.info('job created successfully.', job.toJSON());
 
-    return res.status(status_codes.OK).send({ job: job.toJSON() });
+    return res.status(statusCodes.OK).send({ id: job.id });
 };
 
-exports.getById = (req, res) => {
+exports.getById = async (req, res) => {
+    logger.info('job get request recieved.');
     const jobId = req.body.jobId;
 
-    logger.info('job get request recieved.');
+    console.assert(jobId);
+
+    const job = await jobs.findOne({ where: { id: jobId } });
+    if (!job) {
+        logger.info(`job by for id '${jobId}' not found.`);
+        return res.status(statusCodes.BAD_REQUEST).send({ message: 'job not found.' });
+    }
+
+    logger.info('job details sent back.');
+    return res.status(statusCodes.OK).send(job.toJSON());
 };
