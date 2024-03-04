70cce5b40368c177cd2bbeb2e41d746298565850
shifu-dev <csingh8476@gmail.com>
2024-03-04 23:22:52 +0530

    update: add job apply and get api.

--- backend/routes/job.js
+++ backend/routes/job.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/job');
 
-router
-    .post('/postJob', controller.post)
-    .get('/', controller.getById)
-    .get('/getJobs', controller.getJobs);
+router.post('/postJob', controller.post)
+router.get('/', controller.get)
+router.get('/getJobs', controller.getJobs);
+router.post('/apply', controller.apply);
 
 module.exports = router;
