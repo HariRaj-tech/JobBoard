bbcd64b2e85cff8be6c47726c25bd0cacf9a98f6
shifu-dev <csingh8476@gmail.com>
2024-03-07 18:34:25 +0530

    update: fix job search api.

--- backend/routes/job.js
+++ backend/routes/job.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/job');
 
+router.get('/search', controller.searchJob);
+
 router.post('/', controller.createJob)
 router.get('/', controller.getJobs);
 router.get('/:id', controller.getJob)
 router.post('/:id/applications', controller.createApplication);
 router.get('/:id/applications', controller.getApplications);
-router.get('/search', controller.searchJob);
 
 module.exports = router;
