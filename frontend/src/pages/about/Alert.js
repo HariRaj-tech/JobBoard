4d0ea26645a634d9789fd44a23381182c59e4190
SHIVKESHPANDEY <shivkeshpandey2003@gmail.com>
2024-03-11 13:26:50 +0530

    updated alert notification style

--- frontend/src/pages/about/Alert.js
+++ frontend/src/pages/about/Alert.js
@@ -1,17 +1,14 @@
 import React, { useContext } from 'react'
 import { alertContext } from '../../components/context/Context'
+import './Alert.css'; 
 
 export default function Alert() {
-
     const { alert } = useContext(alertContext);
 
     return (
         alert && (
-            // <div className='mx-96 flex justify-center justify-self-center text-xl px-4 py-3 rounded-lg bg-blue-100 text-black font-bold text-center'>
-            //     {alert}
-            // </div>
-            <div className='flex justify-center items-center'>
-                <div className='w-1/2 text-xl bg-blue-300 text-black font-bold rounded-lg py-3 text-center'>
+            <div className='fixed top-0 left-0 w-full h-1/2 flex justify-center items-center z-50 alert-container'>
+                <div className='w-1/5 text-xl bg-blue-400 text-black font-bold rounded-lg py-3 text-center alert-box'>
                     {alert}
                 </div>
             </div>
