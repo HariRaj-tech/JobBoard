c483dc979f31e6d1c63f5a83fa5aeb66ef42afd6
LovishMalhotra <lovishmalhotra2441@gmail.com>
2024-03-12 17:43:54 +0530

    user navbar toggler setup

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,99 @@
-import React, { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { alertContext } from "./context/Context";
+// import React, { useContext } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { alertContext } from "./context/Context";
 
-export default function Navbar() {
+// export default function Navbar() {
+//   const { showAlert } = useContext(alertContext);
+//   let navigate = useNavigate();
+
+//   const handleLogoutClick = () => {
+//     localStorage.removeItem("id");
+//     navigate("/login");
+//     showAlert("Logout successful");
+//   };
+
+//   return (
+//     <nav className="bg-gray-800 ">
+//       <div className="container mx-auto px-4 flex items-center justify-between py-4 ">
+//         <div className="flex items-center space-x-4">
+//           <Link to="/home" className="text-white text-xl font-bold">
+//             JobBoard
+//           </Link>
+
+//           <Link
+//             to="/home"
+//             className="text-white  text-base hover:text-gray-200"
+//           >
+//             Home
+//           </Link>
+//           <Link
+//             to="/findjob"
+//             className="text-white text-base hover:text-gray-200"
+//           >
+//             FindJob
+//           </Link>
+//           <Link
+//             to="/about"
+//             className="text-white text-base hover:text-gray-200"
+//           >
+//             About
+//           </Link>
+//         </div>
+
+//         <div className="flex items-center space-x-4">
+//           {!localStorage.getItem("id") ? (
+//             <>
+//               <Link
+//                 to="/login"
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+//               >
+//                 Login
+//               </Link>
+//               <Link
+//                 to="/signup"
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+//               >
+//                 Signup
+//               </Link>
+//             </>
+//           ) : (
+//             <>
+//               <Link
+//                 to="/editprofile"
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+//               >
+//                 Edit Profile
+//               </Link>
+//               <Link
+//                 to="/userprofile"
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+//               >
+//                 Profile
+//               </Link>
+//               <Link
+//                 to="/signup"
+//                 onClick={handleLogoutClick}
+//                 className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+//               >
+//                 Logout
+//               </Link>
+//             </>
+//           )}
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+import React, { useContext, useState, useEffect } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { alertContext } from "./context/Context";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+export default function CustomNavbar() {
   const { showAlert } = useContext(alertContext);
   let navigate = useNavigate();
 
@@ -12,75 +103,113 @@ export default function Navbar() {
     showAlert("Logout successful");
   };
 
+  //toggler close when select another page
+  const [expanded, setExpanded] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpanded(false);
+  }, [location]);
+
+  const handleToggle = () => setExpanded(!expanded);
+
   return (
-    <nav className="bg-gray-800 ">
-      <div className="container mx-auto px-4 flex items-center justify-between py-4 ">
-        <div className="flex items-center space-x-4">
-          <Link to="/home" className="text-white text-xl font-bold">
-            JobBoard
-          </Link>
-
-          <Link
-            to="/home"
-            className="text-white  text-base hover:text-gray-200"
-          >
-            Home
-          </Link>
-          <Link
-            to="/findjob"
-            className="text-white text-base hover:text-gray-200"
-          >
-            FindJob
-          </Link>
-          <Link
-            to="/about"
-            className="text-white text-base hover:text-gray-200"
-          >
-            About
-          </Link>
-        </div>
-
-        <div className="flex items-center space-x-4">
-          {!localStorage.getItem("id") ? (
-            <>
-              <Link
-                to="/login"
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
-              >
-                Login
-              </Link>
-              <Link
-                to="/signup"
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
-              >
-                Signup
-              </Link>
-            </>
-          ) : (
+    <>
+      <style>
+        {`
+          .collapse {
+            visibility: visible !important;
+          }
+          .nav-link {
+            color: white !important;
+            font-weight:500 !important;
+          }
+        `}
+      </style>
+      <Navbar
+        expand="lg"
+        expanded={expanded}
+        onToggle={handleToggle}
+        className="bg-gray-800 mx-auto px-4 flex items-center justify-between py-3"
+      >
+        <Navbar.Brand
+          as={Link}
+          to="/home"
+          className="text-white text-xl font-bold"
+        >
+          JobBoard
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto flex-grow-1">
+            <Nav.Link
+              as={Link}
+              to="/home"
+              className="text-white text-base  hover:text-gray-200"
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/findjob"
+              className="text-white text-base hover:text-gray-200"
+            >
+              FindJob
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/about"
+              className="text-white text-base hover:text-gray-200"
+            >
+              About
+            </Nav.Link>
+          </Nav>
+          <Nav className="flex flex-start gap-2 my-auto">
+            {!localStorage.getItem("id") ? (
               <>
                 <Link
+                  to="/login"
+                  className="bg-blue-500 text-white px-4 py-2 my-auto rounded-lg hover:bg-blue-700"
+                >
+                  Login
+                </Link>
+                <Link
+                  as={Link}
+                  to="/signup"
+                  className="bg-blue-500 text-white px-4 py-2 my-1 rounded-lg hover:bg-blue-700"
+                >
+                  Signup
+                </Link>
+              </>
+            ) : (
+              <>
+                <Nav
+                  as={Link}
                   to="/editprofile"
-                  className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
+                  className="bg-blue-500 text-white px-4 my-1 py-2 rounded-lg hover:bg-blue-700"
                 >
                   Edit Profile
-                </Link>
-              <Link
-                to="/userprofile"
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
-              >
-                Profile
-              </Link>
-              <Link
-                to="/signup"
-                onClick={handleLogoutClick}
-                className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700"
-              >
-                Logout
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    </nav>
+                </Nav>
+                <Nav
+                  as={Link}
+                  to="/userprofile"
+                  className="bg-blue-500 text-white px-4 my-1 py-2 rounded-lg hover:bg-blue-700"
+                >
+                  Profile
+                </Nav>
+                <Nav
+                  as={Link}
+                  to="/signup"
+                  onClick={handleLogoutClick}
+                  className="bg-blue-500 text-white px-4 my-1 py-2 rounded-lg hover:bg-blue-700"
+                >
+                  Logout
+                </Nav>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
 }
